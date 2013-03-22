b781f0a50dcffb2be199d073e08ff74ac2875ea5
mdemo <mengdesen09@qq.com>
2013-03-22 21:03:55 +0800

    dfddd

--- js/template.js
+++ js/template.js
@@ -1,7 +1,42 @@
-/**
- * Created with JetBrains WebStorm.
- * User: mdemo
- * Date: 13-3-22
- * Time: 下午7:51
- * To change this template use File | Settings | File Templates.
- */
+var template = $('<div id="husky_chat_bar"><div id="husky_chat_sidebar"><!--Header--><div id="husky_chat_sidebar_header"><div id="husky_chat_sidebar_logo"></div><ul id="husky_chat_sidebar_header_links" style="display:block;"><li id="husky_chat_room_online_users"><a id="husky_chat_sidebar_online_pop_up_toggle" href="#">1 Online</a></li></ul><div id="husky_chat_sidebar_online_pop_up" style="display:none;"><div id="husky_chat_sidebar_online_pop_up_top"></div><div id="husky_chat_sidebar_online_list"><ul id="husky_room_online_users_list"><li id="husky_room_online_username"><a href="#" class="husky_room_online_username_list">Jiahan Wang</a></li></ul></div><div id="husky_chat_sidebar_online_pop_up_bottom"></div></div><ul id="husky_lobby_sidebar_header_links" style="display:none;"><li id="husky_lobby_room_online_users"><a id="husky_lobby_sidebar_online_pop_up_toggle" href="#">1 Online</a></li></ul><div id="husky_lobby_sidebar_online_pop_up" style="display:none;"><div id="husky_lobby_sidebar_online_pop_up_top"></div><div id="husky_lobby_sidebar_online_list"><ul id="husky_lobby_online_users_list"><li id="husky_lobby_online_username"><a href="#" class="husky_lobby_online_username_list">Jiahan Wang</a></li></ul></div><div id="husky_lobby_sidebar_online_pop_up_bottom"></div></div></div><div id="tabs"><ul><li style="margin-right:6px;"><a href="#tabs-1">聊天</a></li><li><a href="#tabs-2">热点</a></li></ul><div id="tabs-1"><div id="husky_chat_sidebar_chatswitch"><div id="husky_chat_sidebar_chatswitch_text">大家在谈论&nbsp;<a href="http://www.baidu.com/s?word=%E6%9D%8E%E5%A4%A9%E4%B8%80" target="_blank">李天一</a></div><!--<a id="husky_chat_sidebar_chatswitch_pagespecific_url" href="#"></a>--></div><div id="husky_chat_sidebar_conversation_window"><div class="slimScrollDiv" style="position:relative;overflow:hidden;width:auto;"><ul id="husky_chat_url_conversation_stream" style="overflow:hidden;width:auto;"><li class="husky_chat_sidebar_message"><div class="husky_chat_sidebar_message_you"><div class="husky_chat_sidebar_message_info"><strong class="husky_chat_sidebar_message_sender"><a href="#" id="husky_chat_sidebar_event_name_username" class="husky_chat_sidebar">Jiahan Wang</a></strong><abbr class="husky_chat_sidebar_message_timestamp">下午1:43:48</abbr></div><div class="husky_chat_sidebar_message_body"><p class="husky_chat_sidebar_message_body_p">Hello there!</p></div></div></li><li class="husky_chat_sidebar_message"><div class="husky_chat_sidebar_message_you"><div class="husky_chat_sidebar_message_info"><strong class="husky_chat_sidebar_message_sender"><a href="#" id="husky_chat_sidebar_event_name_username" class="husky_chat_sidebar">Jiahan Wang</a></strong><abbr class="husky_chat_sidebar_message_timestamp">下午1:43:53</abbr></div><div class="husky_chat_sidebar_message_body"><p class="husky_chat_sidebar_message_body_p">Is anyone here?</p></div></div></li><!--<li class="husky_chat_sidebar_event"><div class="husky_chat_sidebar_event_body"><strong class="husky_chat_sidebar_event_name"><a id="husky_chat_sidebar_event_name_username"></a></strong><b>Desen Meng</b>joined the room. <abbr class="husky_chat_sidebar_event_timestamp">14:23:39</abbr></div></li>--><li class="husky_chat_sidebar_message"><div class="husky_chat_sidebar_message_else"><div class="husky_chat_sidebar_message_info"><strong class="husky_chat_sidebar_message_sender"><a href="#" id="husky_chat_sidebar_event_name_username" class="husky_chat_sidebar">Desen Meng</a></strong><abbr class="husky_chat_sidebar_message_timestamp">下午1:44:55</abbr></div><div class="husky_chat_sidebar_message_body"><p class="husky_chat_sidebar_message_body_p">Hello,nice to meet you</p></div></div></li><li class="husky_chat_sidebar_message"><div class="husky_chat_sidebar_message_you"><div class="husky_chat_sidebar_message_info"><strong class="husky_chat_sidebar_message_sender"><a href="#" id="husky_chat_sidebar_event_name_username" class="husky_chat_sidebar">Jiahan Wang</a></strong><abbr class="husky_chat_sidebar_message_timestamp">下午1:46:41</abbr></div><div class="husky_chat_sidebar_message_body"><p class="husky_chat_sidebar_message_body_p">You are so bored==</p></div></div></li></ul><div class="slimScrollBar ui-draggable" style="background-color:rgb(0,0,0);width:8px;position:absolute;top:0px;opacity:0.4;display:block;border-top-left-radius:8px;border-top-right-radius:8px;border-bottom-right-radius:8px;border-bottom-left-radius:8px;z-index:99;right:1px;height:752px;background-position:initial initial;background-repeat:initial initial;display:none;"></div><div class="slimScrollRail" style="width:8px;height:100%;position:absolute;top:0px;display:none;border-top-left-radius:8px;border-top-right-radius:8px;border-bottom-right-radius:8px;border-bottom-left-radius:8px;background-color:rgb(51,51,51);opacity:0.2;z-index:90;right:1px;background-position:initial initial;background-repeat:initial initial;"></div></div></div><div id="husky_chat_send_message_module" class="send_message_module"><div id="husky_chat_send_message_module_wrap"><textarea id="husky_chat_send_message_module_wrap_textarea" name="message_box"></textarea><a id="husky_chat_send_message_module_sendbutton"></a></div></div></div><div id="tabs-2"><div id="hot_topics_cloud"><!-- <a href="#">标签云</a><a href="#" class="red">PHP</a><a href="#">瀑布流</a><a href="#">Tab</a><a href="#" class="blue">流体布局</a><a href="#">SEO</a><a href="#" class="red">彩蛋</a><a href="#" class="green">JavaScript</a><a href="#">miaov</a><a href="#" class="red">CSS</a><a href="#">asp.net</a><a href="#" class="blue">蓝色经典</a><a href="#">OOP</a><a href="#" class="red">Android</a><a href="#" class="blue">妙味茶馆</a><a href="#">dialog</a><a href="#" class="blue">淘客</a><a href="#">Pin</a><a href="#">微博</a><a href="#" class="red">CSS</a><a href="#">asp.net</a><a href="#" class="blue">蓝色经典</a><a href="#">OOP</a><a href="#" class="red">Android</a><a href="#" class="blue">妙味茶馆</a><a href="#">dialog</a><a href="#" class="blue">淘客</a><a href="#">Pin</a><a href="#">微博</a>--></div></div></div></div></div>');
+var tongzaiDom = $(template);
+$("body").append(tongzaiDom);
+window.baidu = {
+    'hot':  function (data){
+        var $tagCloud = $('#hot_topics_cloud');
+        $tagCloud.children('a').remove();
+        var hot_topics = data.hot;
+        console.log(hot_topics);
+        for(var i = 1 ; i <= 20 ; i ++){
+            var hot_topic = $('<a>');
+            hot_topic.attr("href", hot_topics[i].url);
+            hot_topic.attr("target", "_blank");
+            hot_topic.html(hot_topics[i].word);
+            hot_topic.appendTo($tagCloud);
+
+        }
+        $('#hot_topics_cloud').windstagball({
+            radius:130,
+            speed:6
+        });
+
+    }
+};
+$( "#tabs" ).tabs({
+    //collapsible: true
+});
+
+$('a#ui-id-2').click(function(){
+    $.ajax({
+        type: "get",
+        async: false,
+        url: "http://api.m.baidu.com/?type=hot&from=&dt=jsonp",
+        dataType: "jsonp",
+        jsonpCallback:"hot",
+        error: function(event, jqXHR){
+            //alert(jqXHR);
+        }
+    });
+});
+
+

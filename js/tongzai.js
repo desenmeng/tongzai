1c6630fcebd6c42582266d74bd5fe21a86997c2f
DingDong <522376151@qq.com>
2013-03-23 11:02:45 +0800

    user online

--- js/tongzai.js
+++ js/tongzai.js
@@ -4,17 +4,54 @@
  * Time: 上午11:52
  *
  */
+var $hi,$q,$hiq,$hiqu,$hiquc;
+var getQuery=function(){
+	var reg = window.location.search;
+	var s = reg.split('wd=');
+	var value;
+	if(s.length>0)
+	{
+		var t = s[1].split('&');
+	value=t[0];	
+	}
+	return value;
+}
+
+function refreshUser(){
+	var $user = $("#husky_chat_sidebar_online_pop_up_toggle");
+	if($user[0]){
+		$user[0].innerHTML = $hiquc+' Online';
+	}
+	
+}
 var tongzai = Class.extend({
     init:function(){
-
     }
 });
-var hi = new Firebase('https://hi.firebaseio.com');
-var mdemo = new Firebase('https://hi.firebaseio.com/mdemo');
-mdemo.push($.query.get('wd'));
-mdemo.on("child_added",function(snapshot){
-    console.log(snapshot.val());
-});
+$hi = new Firebase('https://hi.firebaseio.com/');
+$q=getQuery();
+if($q){
+	$hiq=$hi.child($q);
+	$hiqu=$hiq.child('user');
+	$hiqu.on("value",function(dataSnapshot){
+		$hiquc=dataSnapshot.val();
+		if($hiquc){
+			refreshUser();
+			$hiqu.onDisconnect().set($hiquc-1);
+		}
+		console.log('currentUserCount:'+$hiquc);
+	});
+	$hiqu.transaction(function(curUserCount){
+		if(curUserCount == null){
+			return 1;
+		}
+		return curUserCount+1;
+	},function(error,abort,msg){
+		if(error){
+		}
+	});
+}
+
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         switch(request.action) {
@@ -23,3 +60,10 @@ chrome.extension.onMessage.addListener(
                 break;
         }
     });
+
+
+
+
+
+
+
